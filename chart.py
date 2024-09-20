91bcab02cf8edaea2e51db406e64ffd973e5dab9
kulewers <kule.wers@gmail.com>
2024-09-20 13:13:59 +0300

    Simplify chart.py by using Pandas library

--- chart.py
+++ chart.py
@@ -1,11 +1,11 @@
-from sqlalchemy.orm import Session
-import numpy as np
 from matplotlib import pyplot as plt
-from datetime import date
-import datetime
+import pandas as pd
+
 from database import engine
-import models
+
+from datetime import date, timedelta
 import argparse
+import calendar
 
 
 parser = argparse.ArgumentParser(description="Crate a chart of your week's expenses")
@@ -14,35 +14,21 @@ args = parser.parse_args()
 
 
 def main():
-  weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+  # Load data to appropriate format
+  df = pd.read_sql(sql='SELECT id, category, date, amount FROM expense', con=engine, index_col='id')
+  df = df.pivot_table(index='date', columns='category', values='amount', aggfunc='sum', fill_value=0)
 
-  # Create list of days in current week
-  week = [date.today()+datetime.timedelta(days=i-date.today().weekday()) for i in range(7)]
+  # Fill empty dates
+  monday = date.today()-timedelta(days=date.today().weekday())
+  daterange = pd.date_range(start=monday, periods=7, freq='D', inclusive='both', normalize=True)
+  week = [str(t.to_pydatetime().date()) for t in daterange]
+  df = df.reindex(week, fill_value=0)
 
-  # Create dictionary of form
-  # dict[<category>] == array([<mon>, <tue>, <wed>...])
-  with Session(engine) as session:
-    weight_counts = {}
-    # for each category
-    for entry in session.query(models.Expense).group_by(models.Expense.category).distinct():
-      category = entry.category
-      # initiate array with values for each weekday
-      weight_counts[str(category)] = np.zeros(7)
-    for idx, day in enumerate(week):
-      day_expenses = session.query(models.Expense).filter(models.Expense.date == day).all()
-      for expense in day_expenses:
-        weight_counts[expense.category][idx] += expense.amount 
-
-  width = .8
-  _, ax = plt.subplots(figsize=(10, 7))
-  bottom = np.zeros(7)
-
-  for boolean, weight_count in weight_counts.items():
-    ax.bar(weekdays, weight_count, width, label=boolean, bottom=bottom)
-    bottom += weight_count
-
-  ax.set_title("Your expenses during current week")
-  ax.legend(loc="upper right")
+  # Create and style chart
+  ax = df.plot(kind='bar', stacked=True, title="Your expenses this week", xlabel='Day', ylabel='Expenses')
+  weekdays = list(calendar.day_name) 
+  ax.set_xticklabels(weekdays)
+  plt.xticks(rotation=20)
 
   if args.output:
     plt.savefig(f"{args.output}.png")
